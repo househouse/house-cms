293eccc4e34491d6c8960f68d199680021dcef7f
Sean Durfee <sean@house-house.design>
2017-10-25 18:30:40 +0100

    add normalize, nuxt config empty config objects

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,28 @@
 module.exports = {
   build: {},
-  head: {},
+  css: [
+    'normalize.css',
+  ],
+  dev: {},
+  env: {
+    baseUrl: process.env.BASE_URL || 'http://localhost:3000',
+  },
+  generate: {},
+  head: {
+    meta: [
+      { charset: 'utf-8', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+    ],
+  },
   loading: { color: '#3B8070' },
+  loadingIndicator: {},
   manifest: {
     theme_color: '#3B8070',
   },
-  moduels: [
+  modules: [
     '@nuxtjs/pwa',
+    'axios',
   ],
-}
+  plugins: [],
+};
