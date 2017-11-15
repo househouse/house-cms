b1dea8b9bd5670a420f407284fc99f731081a156
Sean Durfee <sean@house-house.design>
2017-11-15 12:37:48 +0000

    refactor requests tool kit

--- api/index.js
+++ api/index.js
@@ -2,132 +2,32 @@
 // wordpress, but just customize the filtered data objects and the baseUrl
 
 import axios from 'axios';
+import humps from 'humps';
 
-export default {
-  baseUrl: 'http://localhost:8888/wp-json/wp/v2/',
-  getPage(slug) {
-    return new Promise((resolve, reject) => {
-      axios.defaults.baseurl = this.baseUrl;
-      axios.get(`pages?slug=${slug}`)
-        .then((response) => {
-          const data = [...response.data];
-          const filtered = {
-            content: data.content.rendered,
-            author: data.author,
-            date: data.date,
-            date_gmt: data.date_gmt,
-            excerpt: data.excerpt.rendered,
-            featured_media: data.featured_media,
-            guid: data.guid.rendered,
-            link: data.link,
-            slug: data.slug,
-            title: data.title.rendered,
-          };
-          resolve(filtered);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
-  },
+export const API_URL = process.env.baseUrl;
 
-  getPost(slug) {
-    return new Promise((resolve, reject) => {
-      axios.defaults.baseurl = this.baseUrl;
-      axios.get(`posts?slug=${slug}`)
-        .then((response) => {
-          const data = [...response.data];
-          const filtered = {
-            content: data.content.rendered,
-            author: data.author,
-            date: data.date,
-            date_gmt: data.date_gmt,
-            excerpt: data.excerpt.rendered,
-            featured_media: data.featured_media,
-            guid: data.guid.rendered,
-            link: data.link,
-            slug: data.slug,
-            title: data.title.rendered,
-          };
-          resolve(filtered);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
-  },
+axios.defaults.baseURL = API_URL;
 
-  getPosts() {
-    return new Promise((resolve, reject) => {
-      axios.defaults.baseurl = this.baseUrl;
-      axios.get('http://localhost:8888/wp-json/wp/v2/posts')
-        .then((response) => {
-          const data = [...response.data];
-          const filtered = {
-            total: response.headers['x-wp-total'],
-            totalPages: response.headers['x-wp-totalpages'],
-            data: data.map(item => ({
-              id: item.id,
-              title: item.title.rendered,
-              content: item.content.rendered,
-              excerpt: item.excerpt.rendered,
-              slug: item.slug,
-            })),
-          };
-          resolve(filtered);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
-  },
-
-  getCategory(slug) {
-    return new Promise((resolve, reject) => {
-      axios.defaults.baseurl = this.baseUrl;
-      axios.get(`categories?slug=${slug}`)
-        .then((response) => {
-          const data = [...response.data];
-          return {
-            id: data.id,
-            name: data.name,
-            slug: data.slug,
-          };
-        })
-        .then((category) => {
-          axios.get(`posts?categories=${category.id}`)
-            .then((response) => {
-              const data = [...response.data];
-              const categoryPosts = data.map(item => ({
-                id: item.id,
-                title: item.title.rendered,
-                content: item.content.rendered,
-                excerpt: item.excerpt.rendered,
-                slug: item.slug,
-              }));
-              resolve(categoryPosts);
-            })
-            .catch((error) => {
-              reject(error);
-            });
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
-  },
-
-  getCategories() {
-    return new Promise((resolve, reject) => {
-      axios.defaults.baseurl = this.baseUrl;
-      axios.get('categories')
-        .then((response) => {
-          const data = [...response.data];
-          resolve(data);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
-  },
+const handleRes = (response, resolve) => {
+  const { data } = response;
+  const camelizedResponse = humps.camelizeKeys(data);
+  resolve(camelizedResponse);
 };
+const handleErr = (error, reject) => {
+  reject(error);
+};
+const request = (requestType, url, data = null) => (
+  new Promise((resolve, reject) => {
+    axios[requestType](url, data)
+      .then((response) => { handleRes(response, resolve); })
+      .catch((error) => { handleErr(error, reject); });
+  })
+);
+
+export const getPage = slug => (request('get', `pages?slug=${slug}`));
+export const getPost = slug => (request('get', `posts?slug=${slug}`));
+export const getPosts = () => (request('get', 'posts'));
+export const getCategory = slug => (request('get', `categories?slug=${slug}`));
+export const getCateogryPosts = id => (request('get', `posts?categories=${id}`));
+
+export const getCategories = () => (request('get', 'categories'));
