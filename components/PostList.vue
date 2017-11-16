aeb1e62a9a580846b0fb87085eaf58e6ec6d7d34
Sean Durfee <sean@house-house.design>
2017-11-16 16:55:37 +0000

    postlist component

---
+++ components/PostList.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div>
+    <div class="post" v-for="post in posts">
+      <h3><nuxt-link :to="slugToUrl(post.slug)">{{ post.title }}</nuxt-link></h3>
+      <div v-html="post.excerpt"></div>
+      <strong class="more"><nuxt-link :to="slugToUrl(post.slug)">read more</nuxt-link></strong>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['posts', 'title'],
+  methods: {
+    slugToUrl(slug) {
+      return `/${slug}`;
+    }
+  },
+  mounted() {
+    this.$store.dispatch('getCategories');
+  },
+}
+</script>
+
+<style lang="css">
+</style>
